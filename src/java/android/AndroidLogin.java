ff16b451fdbf551cbc05d02d33379149c8bc5244
Vitaly <mtgmind@gmail.com>
2013-05-11 15:42:40 +0200

    Comments
    Comments in AndroidLogin and AndroidRegistration servlets

--- src/java/android/AndroidLogin.java
+++ src/java/android/AndroidLogin.java
@@ -18,17 +18,18 @@ import javax.servlet.http.HttpSession;
 import session.UserFacade;
 
 /**
- *
+ * Servlet responsible for Android connection
  * @author Vitaly
  */
 public class AndroidLogin extends HttpServlet {
-
+    //Variables
     private HttpSession httpsession;   
     private String username = null;
     private String password = null;
     @EJB
     private UserFacade userFacade;
     
+    // <editor-fold defaultstate="collapsed" desc="processReques method.">
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -59,12 +60,15 @@ public class AndroidLogin extends HttpServlet {
             out.close();
         }
     }
+    // </editor-fold>
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP
      * <code>GET</code> method.
      *
+     * get processing
+     * method responsible for user login check
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -73,32 +77,37 @@ public class AndroidLogin extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        //Returns the current HttpSession associated with this request
         httpsession = request.getSession();
-        
+        //variables from session
         username = request.getParameter("username");
         password = request.getParameter("password");
-        
+        //converted object used as a response from the server
         ConnectionResult result = new ConnectionResult();
+        //Gson object
         Gson gson = new Gson();
+        //response type
         response.setContentType("application/json");
-        
+        //check if obtained variables not null
         if ((!username.equals(""))&(!password.equals("")))
         {
-            if (checkUser())
+            if (checkUser())//check username and password
             {
                 result.setResult(true);
+                //servlet response
                 response.getWriter().write(gson.toJson(result));    
             }
             else
             {
             	result.setResult(false);
+                //servlet response
                 response.getWriter().write(gson.toJson(result));
             }
         }
         else
         {
         	result.setResult(false);
+                //servlet response
                 response.getWriter().write(gson.toJson(result));
         }
         
@@ -119,6 +128,7 @@ public class AndroidLogin extends HttpServlet {
         processRequest(request, response);
     }
 
+    // <editor-fold defaultstate="collapsed" desc="getServletInfo method.">
     /**
      * Returns a short description of the servlet.
      *
@@ -129,24 +139,27 @@ public class AndroidLogin extends HttpServlet {
         return "Short description";
     }// </editor-fold>
     
-    
+    /**
+     * Method checkUser checks username and password
+     * @return boolean
+     */
     protected boolean checkUser()
     {
         boolean access = false;
-        
+        //get user from database
         User user = userFacade.getUserByUsername(username);
         
-        if(user!=null)
+        if(user!=null)//check if user exists
         {
-            if(user.getPassword().equals(password))
+            if(user.getPassword().equals(password))//checks password of existing user
             {
                 access = true;
+                //save variables to session
                 httpsession.setAttribute("user", user);
                 httpsession.setAttribute("username", username);
                 httpsession.setAttribute("userID", user.getId());
             }
         }
-        
         return access;
     }
 }
--- src/java/android/AndroidRegistration.java
+++ src/java/android/AndroidRegistration.java
@@ -20,15 +20,17 @@ import javax.servlet.http.HttpSession;
 import session.UserFacade;
 
 /**
- *
+ * Servlet is responsible for registration from android client
  * @author Vitaly
  */
 public class AndroidRegistration extends HttpServlet {
-
+    //Variables
     private HttpSession httpsession;
     String action = "";
     @EJB
     UserFacade userFacade;
+    
+    // <editor-fold defaultstate="collapsed" desc="processRequest method.">
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -59,12 +61,15 @@ public class AndroidRegistration extends HttpServlet {
             out.close();
         }
     }
+    // </editor-fold>
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP
      * <code>GET</code> method.
      *
+     * get processing
+     * method check action and use corresponding methods
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -73,32 +78,33 @@ public class AndroidRegistration extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        //Returns the current HttpSession associated with this request
         httpsession = request.getSession();
+        //variables from session, responsible for selecting actions
         action = (String)request.getParameter("instant");
         
         if(action.equals("registration"))
         {
-            registerNewUser(request, response);
+            registerNewUser(request, response);//register new user
         }
         else if(action.equals("username"))
         {
-            checkUsername(request, response);
+            checkUsername(request, response);//check if username is free
         }
         else
         {
             Gson gson = new Gson();
             ConnectionResult result = new ConnectionResult(false);
-            response.getWriter().write(gson.toJson(result));
+            response.getWriter().write(gson.toJson(result));//fail
         }
-        
-        
     }
 
     /**
      * Handles the HTTP
      * <code>POST</code> method.
      *
+     * post processing
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -110,6 +116,7 @@ public class AndroidRegistration extends HttpServlet {
         processRequest(request, response);
     }
 
+    // <editor-fold defaultstate="collapsed" desc="getServletInfo method.">
     /**
      * Returns a short description of the servlet.
      *
@@ -120,50 +127,67 @@ public class AndroidRegistration extends HttpServlet {
         return "Short description";
     }// </editor-fold>
     
+    /**
+     * Method registerNewUser register new user in database
+     * @param request servlet request
+     * @param response servlet response
+     * @throws IOException if an I/O error occurs
+     */
     private void registerNewUser(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
+        //variables
         String registerName;
         String registerMail;
         String registerPassword;
-        
+        //get variables from request
         registerName = request.getParameter("username");
         registerMail = request.getParameter("email");
         registerPassword = request.getParameter("password");
-        
+        //create user
         User user = new User(1);
         user.setUsername(registerName);
         user.setEmail(registerMail);
         user.setPassword(registerPassword);
         user.setActive(false);
         user.setFacebook(false);
-        
+        //save user to database
         userFacade.create(user);
-             
+        //response type
         response.setContentType("application/json");
+        //Google gson object
         Gson gson = new Gson();
+        //converted object used as a response from the server
         ConnectionResult result = new ConnectionResult(true);
+        //servlet response
         response.getWriter().write(gson.toJson(result));
     }
     
+    /**
+     * Method checkUsername method check if username is free on server
+     * @param request servlet request
+     * @param response servlet response
+     * @throws IOException if an I/O error occurs
+     */
     private void checkUsername(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
         String username = (String)request.getParameter("name");
+        //Google gson object
         Gson gson = new Gson();
+        //converted object used as a response from the server
         ConnectionResult result = new ConnectionResult();
-        
+        //get user object from db
         User user = userFacade.getUserByUsername(username);
         if(user==null)
         {
-            result.setResult(true);
+            result.setResult(true);//name is free
         }
         else
         {
-            result.setResult(false);
+            result.setResult(false);//name is occupied
         }
-
+        //responce type
         response.setContentType("application/json");
+        //servlet responce
         response.getWriter().write(gson.toJson(result));
-    }
-    
-    
+    }   
 }
